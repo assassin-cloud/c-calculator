d2427f8fe50a49485ae4ec778196faaa57dd53df
Assassin Spider <raif75219@gmail.com>
2026-09-01 17:38:14 +0500

    Implement user login and advanced calculations
    Added user authentication and enhanced calculator functionality.

--- calculator.cpp
+++ calculator.cpp
@@ -2,40 +2,93 @@
 using namespace std;
 
 int main() {
-    double a;
-    double b;
-    char calc;
+    double a, b;
+    string user, pass;
+    string calc, end;
+
+    cout << "Input username: " << endl;
+    cin >> user;
+    if(user == "admin" || user == "ADMIN") {
+       cout << "input password: " << endl;
+       cin >> pass;
+
+       if(pass == "root" || pass == "ROOT") {
+        cout << "To continue type 'y'or 'q' to exit:" << endl;
+        cin >> end;
+    
+    while (end == "y") {
+
+        
+    cout << "input calculation symbol e.g + - / * or s for square root and c for cube root: " << endl;
+    cin >> calc;
+    if(calc == "s"){
+        double *p = new double;
+        cout << "Input number: ";
+        cin >> *p;
+        cout << endl;
+        cout << "answer: " << (*p)*(*p) << endl;
+        delete p;
+        p = nullptr;
+        cout << "To continue type 'y'or 'q' to exit:" << endl;
+        cin >> end;
+    }
+    else if(calc == "c"){
+        double *ptr = new double;
+        cout << "input number: ";
+        cin >> *ptr;
+        cout << "answer: " << (*ptr)*(*ptr)*(*ptr) << endl;
+        delete ptr;
+        ptr = nullptr;
+        cout << "To continue type 'y'or 'q' to exit:" << endl;
+        cin >> end;
+    }
+    else if(calc == "+" || calc == "-" || calc == "/" || calc == "*"){
+
     cout << "input first number, a: " << endl;
     cin >> a;
 
     cout << "input second number, b: " << endl;
     cin >> b;
-
-    cout << "input calculation symbol e.g + - / *: " << endl;
-    cin >> calc;
        
-    switch (calc) {
-        case '+':
+    if(calc == "+") {
         cout << "Solution: " << endl;
-        cout << a + b << endl;
-        break;
+        cout << a+b << endl;
+    }
 
-        case '-':
+    else if(calc == "-"){
         cout << "Solution: " << endl;
-        cout << a - b << endl;
-        break;
+        cout << a-b << endl;
+    }
 
-        case '/':
+    else if(calc == "/"){
+        if(b == 0) {
+        	cout << "Can't divide by zero" << endl;
+        	break;
+        }
+        else{
         cout << "Solution: " << endl;
-        cout << a / b << endl;
-        break;
+        cout << a/b << endl;
+        }
+    }
 
-        case '*':
+    else if(calc == "*"){
         cout << "Solution: " << endl;
-        cout << a * b << endl;
-        break;
+        cout << a*b << endl;
+    }
+    cout << "type q to quit or type y to continue" << endl;
+    cin >> end;
+    }
+    else{
+        cout << "Invalid Input" << endl;
+    }
+    }
 
-        default:
-        cout << "Invalid symbol";
+       }
+       else {
+        cout << "Invalid password";
+       }
+    }
+    else {
+        cout << "Invalid username";
     }
 }
