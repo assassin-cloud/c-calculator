05dc6ed1d81b6de60a681d72a9fbc221929f4819
Assassin Spider <raif75219@gmail.com>
2026-09-20 16:37:31 +0500

    Add files via upload

---
+++ calculator/function.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<string>
+void welcome();
+struct contact;
+int getinput();
+void listcontact(int numberofcontact);
+void addcontact(int&numberofcontact);
+void deletecontact(int&numberofcontact);
+void bugfix();
+void goback();
